1ef381bc923a681c6b619d6e93a456dbc97ea053
David Jones <davidthomjones@gmail.com>
2010-05-26 14:45:27 +1000

    Don't know what I did here ;) - but this file will disappear eventually

--- staff/staffActivity.php
+++ staff/staffActivity.php
@@ -46,7 +46,35 @@ class staffActivity extends Indicator {
          //The Hits graph
          $text = "";
 
-         $text = "<br />Staff Course Activity";
+         $text =  <<<EOF
+<script type="text/javascript" src="/protovis/protovis-r3.1.js"></script>
+
+<script type="text/javascript+protovis">
+// Create the root panel and set the visualization's size to 150x150
+var vis = new pv.Panel()
+    .width(150)
+    .height(150);
+ 
+// Add the horizontal rules (grid lines), we add them first so they go in the back.
+vis.add(pv.Rule)
+    .data(pv.range(0, 3, .5))
+    .bottom(function(d) d * 80 + 1)
+  .add(pv.Label);
+ 
+// Add the bars with the height corresponding to the values in the data property
+vis.add(pv.Bar)
+    .data([2, 1.2, 1.7, 1.5, .7])
+    .width(20)
+    .height(function(d) 80 * d)
+    .bottom(0)
+    .left(function() this.index * 25 + 25) // this.index is the position of the datum in the array
+  .anchor("bottom").add(pv.Label); // Add a label to the bottom of each bar
+ 
+// Render everything.
+vis.render();
+</script>
+EOF;
+/*         $text = "<br />Staff Course Activity";
          $staffaverage=1;  $allstaffaverage=1;
          $muliplier=(100/(2*$staffaverage));
          $staffhitsresult=round($staffresult*$muliplier);
@@ -57,7 +85,7 @@ class staffActivity extends Indicator {
          $muliplier=(100/(2*$allstaffaverage));
          $staffpostresult=round($staffposts*$muliplier);
          $text .= "<br><img src=\"http://chart.apis.google.com/chart?chs=170x70&chd=t:$staffpostresult&cht=gom&chf=bg,s,EFEFEF&chxt=x,y&chxl=0:||1:|Low||High\"></img>";
-
+*/
          return $text;
 
     }
