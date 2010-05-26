7d725f3f0f2d7bd9f2befcad87aff75bd65921ac
David Jones <davidthomjones@gmail.com>
2010-05-26 14:30:18 +1000

    Moving to model/view for indicators

--- factory.php
+++ factory.php
@@ -12,21 +12,41 @@ abstract class Indicator {
 
 class IndicatorFactory {
     
-    public static function create( $context ) {
+    public static function createModel( $context ) {
 
         global $CFG;
         
         if ( has_capability( 'moodle/legacy:teacher', $context ) ||
              has_capability( 'moodle/legacy:editingteacher', $context ) ) {
             require_once( 
-                    $CFG->dirroot.'/blocks/indicators/staff/staffActivity.php');
-            return new staffActivity;
+                    $CFG->dirroot.'/blocks/indicators/staff/activity/model.php');
+            return new activityModel;
         } else if ( has_capability( 'moodle/legacy:student', $context ) ) {
             require_once( 
-                    $CFG->dirroot.'/blocks/indicators/student/studentActivity.php');
-            return new studentActivity;
+                    $CFG->dirroot.'/blocks/indicators/student/activity/model.php');
+            return new activityModel;
         } 
     }
+
+    # - create the view object
+    # - currently use Protovis for students and Google for staff
+
+    public static function createView( $model, $context ) {
+
+        global $CFG;
+
+        if ( has_capability( 'moodle/legacy:teacher', $context ) ||
+             has_capability( 'moodle/legacy:editingteacher', $context ) ) {
+            require_once(
+                    $CFG->dirroot.'/blocks/indicators/staff/activity/google_view.php');
+            return new activityView( $model );
+        } else if ( has_capability( 'moodle/legacy:student', $context ) ) {
+            require_once(                    
+                $CFG->dirroot.'/blocks/indicators/student/activity/protovis_view.php');
+            return new activityView( $model );
+        }
+    }
+
 }
 
 ?>
