9ffc695df0ee00bd8c2f5aa29969392abb15e5b3
David Jones <davidthomjones@gmail.com>
2010-05-26 14:45:11 +1000

    Added dummy use of protovis

--- staff/activity/protovis_view.php
+++ staff/activity/protovis_view.php
@@ -13,14 +13,36 @@ class activityView {
        $staffhits = $this->model->staffhitsresult;
        $staffposts = $this->model->staffpostresult;
 
-         $text = "<br />Staff Course Activity";
-         $text .= "<br /><img src=\"http://chart.apis.google.com/chart?chs=170x70&chd=t:$staffhits&cht=gom&chf=bg,s,EFEFEF&chxt=x,y&chxl=0:||1:|Low||High\"></img>";
+         $text =  <<<EOF
+<script type="text/javascript" src="/protovis/protovis-r3.1.js"></script>
 
-         //The forums graph
-         $text .= "<br>Staff Forum Participation";
-         $text .= "<br><img src=\"http://chart.apis.google.com/chart?chs=170x70&chd=t:$staffposts&cht=gom&chf=bg,s,EFEFEF&chxt=x,y&chxl=0:||1:|Low||High\"></img>";
-//print "$text";
-         return $text;
+<script type="text/javascript+protovis">
+// Create the root panel and set the visualization's size to 150x150
+var vis = new pv.Panel()
+    .width(150)
+    .height(150);
+
+// Add the horizontal rules (grid lines), we add them first so they go in the back.
+vis.add(pv.Rule)
+    .data(pv.range(0, 3, .5))
+    .bottom(function(d) d * 80 + 1)
+  .add(pv.Label);
+
+// Add the bars with the height corresponding to the values in the data property
+vis.add(pv.Bar)
+    .data([2, 1.2, 1.7, 1.5, .7])
+    .width(20)
+    .height(function(d) 80 * d)
+    .bottom(0)
+    .left(function() this.index * 25 + 25) // this.index is the position of the datum in the array
+  .anchor("bottom").add(pv.Label); // Add a label to the bottom of each bar
+
+// Render everything.
+vis.render();
+</script>
+EOF;
+
+       return $text;
 
     }
 
